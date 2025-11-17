a8fe0d7dcb14dc9f94b33b49ca9fb49e0c2fa76b
Oleksandr Matskevych <ofreg13@gmail.com>
2025-11-17 11:01:33 +0200

    Lab 5

---
+++ app/string.py
@@ -0,0 +1,6 @@
+def reverse_string(s: str) -> str:
+    return s[::-1]
+
+def is_palindrome(s: str) -> bool:
+    s_clean = ''.join(c.lower() for c in s if c.isalnum())
+    return s_clean == s_clean[::-1]
