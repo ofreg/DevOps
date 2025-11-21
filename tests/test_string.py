da2c4d410d572def752e77a7d835d887d38fcd0f
Oleksandr Matskevych <ofreg13@gmail.com>
2025-11-21 18:37:24 +0200

    lab6

--- tests/test_string.py
+++ tests/test_string.py
@@ -1,8 +1,10 @@
 from app.string import reverse_string, is_palindrome
 
+
 def test_reverse_string():
     assert reverse_string("abc") == "cba"
     assert reverse_string("") == ""
+
 
 def test_is_palindrome():
     assert is_palindrome("A man a plan a canal Panama") is True
